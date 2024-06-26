276f36c2638f48ca47d477dd10a2e9e6af11ecc5
Han Sol Jin <hansol@hansol.ca>
2024-06-26 09:42:07 -0700

    flashlmdd: add bdwlan firmware symlinks
    Change-Id: I33e54d9057810f9dc80941ffe5af8c4e42111a07

--- flashlmdd-vendor.mk
+++ flashlmdd-vendor.mk
@@ -931,3 +931,8 @@ PRODUCT_COPY_FILES += \
     vendor/lge/flashlmdd/proprietary/vendor/lib64/sensors.sar.so:$(TARGET_COPY_OUT_VENDOR)/lib64/sensors.sar.so \
     vendor/lge/flashlmdd/proprietary/vendor/lib64/sensors.ssc.so:$(TARGET_COPY_OUT_VENDOR)/lib64/sensors.ssc.so \
     vendor/lge/flashlmdd/proprietary/vendor/lib64/vendor.lge.hardware.sensors@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.lge.hardware.sensors@1.0.so
+
+PRODUCT_PACKAGES += \
+    bdwlan_bdwlan_symlink \
+    bdwlan_ch0_bdwlan_ch0_symlink \
+    bdwlan_ch1_bdwlan_ch1_symlink
